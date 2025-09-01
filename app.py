b86f890bf4bdf8bbb032540b77c89617ffe0d3fc
Vasiliy Katsyka <114024943+Vasiliy-katsyka@users.noreply.github.com>
2025-09-01 20:47:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -171,7 +171,8 @@ BOOSTED_LUCK_USERS = {
     8262163216: 10,
     1090125517: 1.1,
     7617582277: 1.1,
-    8098040157: 1.1
+    8098040157: 1.1,
+    512257998: 1.1,
 }
 # Define what counts as a "valuable" prize (e.g., worth more than the case price)
 VALUABLE_PRIZE_THRESHOLD_MULTIPLIER = Decimal('1.0')
